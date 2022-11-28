92899565011df99ca2c4ace8ee864ba3a0228ddc
nono <nocurely@gmail.com>
2022-11-28 16:57:12 +0100

    Default page Auth for display navbar

---
+++ Flora/Pages/Auth.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import Navbar from "../Component/Base/Navbar";
+
+const Auth = () => {
+    return (
+        <View>
+            <Navbar/>
+        </View>
+    );
+}
+
+export default Auth;
